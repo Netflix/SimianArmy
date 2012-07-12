46f2e5a970a1984a00fce1b853f375f970196415
Cory Bennett <github@corybennett.org>
2012-07-12 09:31:28 -0700

    fix access to ctors

--- src/main/java/com/netflix/simianarmy/basic/BasicChaosCrawler.java
+++ src/main/java/com/netflix/simianarmy/basic/BasicChaosCrawler.java
@@ -43,13 +43,13 @@ public class BasicChaosCrawler implements ChaosCrawler {
     public static class BasicInstanceGroup implements InstanceGroup {
         private final String name;
         private final Enum type;
-
-        BasicInstanceGroup(String name) {
+        
+        public BasicInstanceGroup(String name) {
             this.name = name;
             this.type = Types.ASG;
         }
 
-        BasicInstanceGroup(String name, Enum type) {
+        public BasicInstanceGroup(String name, Enum type) {
             this.name = name;
             this.type = type;
         }
