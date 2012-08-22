8e97c39013a884c2cbc64aece96ffa79c90120b3
IngmarKrusch <krusch@gmx.de>
2012-08-22 19:16:15 +0200

    added javadoc and copyright

--- src/main/java/com/netflix/simianarmy/client/libvirt/VSphereClient.java
+++ src/main/java/com/netflix/simianarmy/client/libvirt/VSphereClient.java
@@ -20,6 +20,27 @@ import com.vmware.vim25.mo.ManagedEntity;
 import com.vmware.vim25.mo.ServiceInstance;
 import com.vmware.vim25.mo.VirtualMachine;
 
+/*
+ *  Copyright 2012 Immobilienscout GmbH
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+/**
+ * This client describes the AutoScalingGroup's and can terminate Instance's that
+ * are hosted in a VSphere Center.
+ * 
+ * @author ingmar.krusch@immobilienscout24.de
+ */
 public class VSphereClient extends AWSClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(VSphereClient.class);
 
