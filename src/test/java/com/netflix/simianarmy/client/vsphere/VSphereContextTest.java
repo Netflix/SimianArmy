8ac6d424c2d129e8aaa5e8c065158a1304d3a17d
IngmarKrusch <krusch@gmx.de>
2012-09-05 18:46:48 +0200

    Added Test

---
+++ src/test/java/com/netflix/simianarmy/client/vsphere/VSphereContextTest.java
@@ -0,0 +1,37 @@
+// CHECKSTYLE IGNORE Javadoc
+package com.netflix.simianarmy.client.vsphere;
+
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import com.netflix.simianarmy.client.aws.AWSClient;
+
+/*
+ *  Copyright 2012 Immobilienscout GmbH
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+/**
+ * @author ingmar.krusch@immobilienscout24.de
+ */
+public class VSphereContextTest {
+    @Test
+    public void shouldSetClientOfCorrectType() {
+        VSphereContext context = new VSphereContext();
+        AWSClient awsClient = context.getAwsClient();
+        assertNotNull(awsClient);
+        assertTrue(awsClient instanceof VSphereClient);
+    }
+}
