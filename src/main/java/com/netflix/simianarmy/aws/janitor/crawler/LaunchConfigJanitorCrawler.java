0d6b3e468feda4f62a6563a7f190629540fd393e
michaelf <michaelf@netflix.com>
2013-01-22 15:47:16 -0800

    Support launch configuration cleanup by Janitor Monkey

---
+++ src/main/java/com/netflix/simianarmy/aws/janitor/crawler/LaunchConfigJanitorCrawler.java
@@ -0,0 +1,98 @@
+/*
+ *
+ *  Copyright 2012 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
+package com.netflix.simianarmy.aws.janitor.crawler;
+
+import com.amazonaws.services.autoscaling.model.AutoScalingGroup;
+import com.amazonaws.services.autoscaling.model.LaunchConfiguration;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.netflix.simianarmy.Resource;
+import com.netflix.simianarmy.aws.AWSResource;
+import com.netflix.simianarmy.aws.AWSResourceType;
+import com.netflix.simianarmy.client.aws.AWSClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * The crawler to crawl AWS launch configurations for janitor monkey.
+ */
+public class LaunchConfigJanitorCrawler extends AbstractAWSJanitorCrawler {
+
+    /** The name representing the additional field name of a flag indicating if the launch config
+     * if used by an auto scaling group. */
+    public static final String LAUNCH_CONFIG_FIELD_USED_BY_ASG = "USED_BY_ASG";
+
+    /** The Constant LOGGER. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(LaunchConfigJanitorCrawler.class);
+
+    /**
+     * Instantiates a new basic launch configuration crawler.
+     * @param awsClient
+     *            the aws client
+     */
+    public LaunchConfigJanitorCrawler(AWSClient awsClient) {
+        super(awsClient);
+    }
+
+    @Override
+    public EnumSet<?> resourceTypes() {
+        return EnumSet.of(AWSResourceType.LAUNCH_CONFIG);
+    }
+
+    @Override
+    public List<Resource> resources(Enum resourceType) {
+        if ("LAUNCH_CONFIG".equals(resourceType.name())) {
+            return getLaunchConfigResources();
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Resource> resources(String... resourceIds) {
+        return getLaunchConfigResources(resourceIds);
+    }
+
+    private List<Resource> getLaunchConfigResources(String... launchConfigNames) {
+        List<Resource> resources = Lists.newArrayList();
+
+        AWSClient awsClient = getAWSClient();
+
+        Set<String> usedLCs = Sets.newHashSet();
+        for (AutoScalingGroup asg : awsClient.describeAutoScalingGroups()) {
+            usedLCs.add(asg.getLaunchConfigurationName());
+        }
+
+        for (LaunchConfiguration launchConfiguration : awsClient.describeLaunchConfigurations(launchConfigNames)) {
+            String lcName = launchConfiguration.getLaunchConfigurationName();
+            Resource lcResource = new AWSResource().withId(lcName)
+                    .withRegion(getAWSClient().region()).withResourceType(AWSResourceType.LAUNCH_CONFIG)
+                    .withLaunchTime(launchConfiguration.getCreatedTime());
+            lcResource.setOwnerEmail(getOwnerEmailForResource(lcResource));
+
+            lcResource.setAdditionalField(LAUNCH_CONFIG_FIELD_USED_BY_ASG, String.valueOf(usedLCs.contains(lcName)));
+            resources.add(lcResource);
+        }
+        return resources;
+    }
+}
---
+++ src/main/java/com/netflix/simianarmy/aws/janitor/rule/launchconfig/OldUnusedLaunchConfigRule.java
@@ -0,0 +1,104 @@
+/*
+ *
+ *  Copyright 2012 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
+package com.netflix.simianarmy.aws.janitor.rule.launchconfig;
+
+import com.netflix.simianarmy.Resource;
+import com.netflix.simianarmy.MonkeyCalendar;
+import com.netflix.simianarmy.aws.AWSResource;
+import com.netflix.simianarmy.aws.janitor.crawler.LaunchConfigJanitorCrawler;
+import com.netflix.simianarmy.janitor.Rule;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.Validate;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+/**
+ * The rule for detecting the launch configurations that
+ * 1) have been created for certain days and
+ * 2) are not used by any auto scaling groups.
+ */
+public class OldUnusedLaunchConfigRule implements Rule {
+    /** The Constant LOGGER. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(OldUnusedLaunchConfigRule.class);
+
+    private static final String TERMINATION_REASON = "Launch config is not used by any ASG";
+
+    private final MonkeyCalendar calendar;
+
+    private final int ageThreshold;
+
+    private final int retentionDays;
+
+    /**
+     * Constructor for OrphanedInstanceRule.
+     *
+     * @param calendar
+     *            The calendar used to calculate the termination time
+     * @param ageThreshold
+     *            The number of days that a launch configuration is considered as a cleanup canididate
+     *            since it is created
+     * @param retentionDays
+     *            The number of days that the unused launch configuration is retained before being terminated
+     */
+    public OldUnusedLaunchConfigRule(MonkeyCalendar calendar, int ageThreshold, int retentionDays) {
+        Validate.notNull(calendar);
+        Validate.isTrue(ageThreshold >= 0);
+        Validate.isTrue(retentionDays >= 0);
+        this.calendar = calendar;
+        this.ageThreshold = ageThreshold;
+        this.retentionDays = retentionDays;
+    }
+
+    @Override
+    public boolean isValid(Resource resource) {
+        Validate.notNull(resource);
+        if (!"LAUNCH_CONFIG".equals(resource.getResourceType().name())) {
+            return true;
+        }
+        AWSResource lcResource = (AWSResource) resource;
+        String usedByASG = lcResource.getAdditionalField(LaunchConfigJanitorCrawler.LAUNCH_CONFIG_FIELD_USED_BY_ASG);
+        if (StringUtils.isNotEmpty(usedByASG) && !Boolean.parseBoolean(usedByASG)) {
+            if (resource.getLaunchTime() == null) {
+                LOGGER.error(String.format("The launch config %s has no creation time.", resource.getId()));
+                return true;
+            } else {
+                DateTime launchTime = new DateTime(resource.getLaunchTime().getTime());
+                DateTime now = new DateTime(calendar.now().getTimeInMillis());
+                if (now.isBefore(launchTime.plusDays(ageThreshold))) {
+                    LOGGER.info(String.format("The unused launch config %s has not been created for more than %d days",
+                            resource.getId(), ageThreshold));
+                    return true;
+                }
+                LOGGER.info(String.format("The unused launch config %s has been created for more than %d days",
+                        resource.getId(), ageThreshold));
+                if (resource.getExpectedTerminationTime() == null) {
+                    Date terminationTime = calendar.getBusinessDay(new Date(now.getMillis()), retentionDays);
+                    resource.setExpectedTerminationTime(terminationTime);
+                    resource.setTerminationReason(TERMINATION_REASON);
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
