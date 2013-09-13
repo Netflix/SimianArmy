1f1855d14b345d94ce587fce0083199900b2f2a1
Justin Santa Barbara <justin@fathomdb.com>
2013-09-13 15:00:59 -0700

    Log if detach volumes wasn't able to run

--- src/main/java/com/netflix/simianarmy/chaos/DetachVolumesChaosType.java
+++ src/main/java/com/netflix/simianarmy/chaos/DetachVolumesChaosType.java
@@ -2,16 +2,22 @@ package com.netflix.simianarmy.chaos;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.netflix.simianarmy.CloudClient;
+import com.netflix.simianarmy.basic.chaos.BasicChaosMonkey;
 
 /**
  * We force-detach all the EBS volumes.
  *
- * This is supposed to simulate a catastrophic failure of EBS, however the
- * instance will (possibly) still keep running; e.g. it should continue to
- * respond to pings.
+ * This is supposed to simulate a catastrophic failure of EBS, however the instance will (possibly) still keep running;
+ * e.g. it should continue to respond to pings.
  */
 public class DetachVolumesChaosType extends ChaosType {
+    /** The Constant LOGGER. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(BasicChaosMonkey.class);
+
     /**
      * Singleton instance of this chaos type.
      */
@@ -30,6 +36,9 @@ public class DetachVolumesChaosType extends ChaosType {
     @Override
     public boolean canApply(CloudClient cloudClient, String instanceId) {
         List<String> volumes = cloudClient.listAttachedVolumes(instanceId, false);
+        if (volumes.isEmpty()) {
+            LOGGER.debug("Can't apply strategy: no non-root EBS volumes");
+        }
         return !volumes.isEmpty();
     }
 
