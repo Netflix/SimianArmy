19da83bfc84de2644a89109e87d356583148c2b7
Justin Santa Barbara <justin@fathomdb.com>
2013-09-27 11:59:57 -0700

    Add other S3 endpoints, per review by coryb

--- src/main/resources/scripts/fails3.sh
+++ src/main/resources/scripts/fails3.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# See http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
+
 echo "127.0.0.1 s3.amazonaws.com" >> /etc/hosts
+echo "127.0.0.1 s3-external-1.amazonaws.com" >> /etc/hosts
+echo "127.0.0.1 s3-us-west-1.amazonaws.com" >> /etc/hosts
+echo "127.0.0.1 s3-us-west-2.amazonaws.com" >> /etc/hosts
+echo "127.0.0.1 s3-eu-west-1.amazonaws.com" >> /etc/hosts
+echo "127.0.0.1 s3-ap-southeast-1.amazonaws.com" >> /etc/hosts
+echo "127.0.0.1 s3-ap-southeast-2.amazonaws.com" >> /etc/hosts
+echo "127.0.0.1 s3-ap-northeast-1.amazonaws.com" >> /etc/hosts
+echo "127.0.0.1 s3-sa-east-1.amazonaws.com" >> /etc/hosts
 
