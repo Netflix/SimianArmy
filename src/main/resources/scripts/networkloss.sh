0776d7674381d8f21f6e6345610757b0bffb383d
archthegit <archieprad@gmail.com>
2018-07-01 15:49:40 +0800

    added sudo to the script to allow for root access

--- src/main/resources/scripts/networkloss.sh
+++ src/main/resources/scripts/networkloss.sh
@@ -3,6 +3,6 @@
 
 # Drops 7% of packets, with 25% correlation with previous packet loss
 # 7% is high, but it isn't high enough that TCP will fail entirely
-tc qdisc add dev eth0 root netem loss 7% 25%
+sudo tc qdisc add dev eth0 root netem loss 7% 25%
 
 
